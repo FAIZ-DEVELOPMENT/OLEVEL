007fae26618ab39dadc47653839283dd53a6272c
faizsaifaiz786@gmail.com <your.email@example.com>
2025-05-06 11:57:35 +0530

    IT

---
+++ IT-Test/ALL IT/itscript.js
@@ -0,0 +1,17 @@
+const totalTests = 32;
+const currentPage = window.location.pathname.split("/").pop(); // e.g., it-5.html
+const sidebar = document.querySelector('.sidebar');
+
+for (let i = 1; i <= totalTests; i++) {
+    const fileName = `it-${i}.html`;
+    if (fileName !== currentPage) {
+        const a = document.createElement("a");
+        a.href = fileName;
+        a.className = "mock-link";
+        a.textContent = `Mock Test ${i}`;
+        sidebar.appendChild(a);
+
+        const hr = document.createElement("hr");
+        sidebar.appendChild(hr);
+    }
+}
